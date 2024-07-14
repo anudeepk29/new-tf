bbe58173ed6700c2f318b88fca0e0d5ca9fc11ee
anudeepk29 <46772745+anudeepk29@users.noreply.github.com>
2024-07-14 19:55:06 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "my_ec2" {
+  ami           = "${var.ec2_ami}"
+  instance_type = "${var.instance_type}"
+  count = "${var.ec2_count}"
+  key_name = "${var.ec2_key}"
+  security_groups = "${var.ec2_sg}"
+  tags = {
+	Name = "${var.server_tag}"
+  }
+
+
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "ec2_ami" {}
+variable "server_tag" {}
+variable "ec2_key" {} 
+variable "ec2_count" {}
+variable "ec2_sg" {}
+variable "instance_type" {}
